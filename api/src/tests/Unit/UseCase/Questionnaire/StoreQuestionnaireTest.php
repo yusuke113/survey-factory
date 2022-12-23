1c7666dd77fdf642d346a34b74d70bf758634545
Yusuke Sato <yusuke.engineer@gmail.com>
2022-12-24 05:54:28 +0900

    test:アンケートの選択肢とタグを新規登録するテスト作成

--- api/src/tests/Unit/UseCase/Questionnaire/StoreQuestionnaireTest.php
+++ api/src/tests/Unit/UseCase/Questionnaire/StoreQuestionnaireTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\UseCase\Questionnaire;
 
+use App\Models\QreChoice;
 use App\Models\Questionnaire;
 use App\Models\Tag;
 use App\Models\User;
@@ -85,7 +86,7 @@ class StoreQuestionnaireTest extends TestCase
         return [
             '正常系' => [
                 '入力値' => [
-                    'userId' => 1,
+                    'userId' => self::USER['id'],
                     'title' => 'アンケートタイトルA',
                     'description' => 'アンケート説明文A',
                     'thumbnailUrl' => 'https://test.jpg',
@@ -101,17 +102,17 @@ class StoreQuestionnaireTest extends TestCase
                     ],
                     'tags' => [
                         [
-                            'id' => 1,
-                            'name' => 'アニメ',
+                            'id' => self::TAGS[0]['id'],
+                            'name' => self::TAGS[0]['name'],
                         ],
                         [
-                            'id' => 2,
-                            'name' => 'ゲーム',
+                            'id' => self::TAGS[1]['id'],
+                            'name' => self::TAGS[1]['name'],
                         ],
                     ],
                 ],
                 '期待値' => [
-                    'user_id' => 1,
+                    'user_id' => self::USER['id'],
                     'title' => 'アンケートタイトルA',
                     'description' => 'アンケート説明文A',
                     'thumbnail_url' => 'https://test.jpg',
@@ -119,7 +120,7 @@ class StoreQuestionnaireTest extends TestCase
             ],
             'サムネイル画像がない場合' => [
                 '入力値' => [
-                    'userId' => 1,
+                    'userId' => self::USER['id'],
                     'title' => 'アンケートタイトルA',
                     'description' => 'アンケート説明文A',
                     'thumbnailUrl' => null,
@@ -135,20 +136,148 @@ class StoreQuestionnaireTest extends TestCase
                     ],
                     'tags' => [
                         [
-                            'id' => 1,
-                            'name' => 'アニメ',
+                            'id' => self::TAGS[0]['id'],
+                            'name' => self::TAGS[0]['name'],
                         ],
                         [
-                            'id' => 2,
-                            'name' => 'ゲーム',
+                            'id' => self::TAGS[1]['id'],
+                            'name' => self::TAGS[1]['name'],
                         ],
                     ],
                 ],
                 '期待値' => [
-                    'user_id' => 1,
+                    'user_id' => self::USER['id'],
                     'title' => 'アンケートタイトルA',
                     'description' => 'アンケート説明文A',
                     'thumbnail_url' => null,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider アンケートに紐づく選択肢を新規登録できること_provider
+     *
+     * @return void
+     */
+    public function アンケートに紐づく選択肢を新規登録できること($input, $expected): void
+    {
+        ($this->useCase)(...$input);
+
+        $actual = Questionnaire::latest()->first()->qreChoices->map(
+            fn (QreChoice $qreChoice): array => Arr::only($qreChoice->toArray(), array_keys($expected[0]))
+        )->all();
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function アンケートに紐づく選択肢を新規登録できること_provider(): array
+    {
+        return [
+            '正常系' => [
+                '入力値' => [
+                    'userId' => self::USER['id'],
+                    'title' => 'アンケートタイトルA',
+                    'description' => 'アンケート説明文A',
+                    'thumbnailUrl' => 'https://test.jpg',
+                    'qreChoices' => [
+                        [
+                          'body' => '選択肢内容A',
+                          'displayOrder' => 1
+                        ],
+                        [
+                          'body' => '選択肢内容B',
+                          'displayOrder' => 2,
+                        ],
+                    ],
+                    'tags' => [
+                        [
+                            'id' => self::TAGS[0]['id'],
+                            'name' => self::TAGS[0]['name'],
+                        ],
+                        [
+                            'id' => self::TAGS[1]['id'],
+                            'name' => self::TAGS[1]['name'],
+                        ],
+                    ],
+                ],
+                '期待値' => [
+                    [
+                        'body' => '選択肢内容A',
+                        'display_order' => 1
+                    ],
+                    [
+                        'body' => '選択肢内容B',
+                        'display_order' => 2,
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider アンケートに紐づくタグを新規登録できること_provider
+     *
+     * @return void
+     */
+    public function アンケートに紐づくタグを新規登録できること($input, $expected): void
+    {
+        ($this->useCase)(...$input);
+
+        $actual = Questionnaire::latest()->first()->tags->map(
+            fn (Tag $tag): array => Arr::only($tag->toArray(), array_keys($expected[0]))
+        )->all();
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function アンケートに紐づくタグを新規登録できること_provider(): array
+    {
+        return [
+            '正常系' => [
+                '入力値' => [
+                    'userId' => self::USER['id'],
+                    'title' => 'アンケートタイトルA',
+                    'description' => 'アンケート説明文A',
+                    'thumbnailUrl' => 'https://test.jpg',
+                    'qreChoices' => [
+                        [
+                          'body' => '選択肢内容A',
+                          'displayOrder' => 1
+                        ],
+                        [
+                          'body' => '選択肢内容B',
+                          'displayOrder' => 2,
+                        ],
+                    ],
+                    'tags' => [
+                        [
+                            'id' => self::TAGS[0]['id'],
+                            'name' => self::TAGS[0]['name'],
+                        ],
+                        [
+                            'id' => self::TAGS[1]['id'],
+                            'name' => self::TAGS[1]['name'],
+                        ],
+                    ],
+                ],
+                '期待値' => [
+                    [
+                        'id' => self::TAGS[0]['id'],
+                        'name' => self::TAGS[0]['name'],
+                    ],
+                    [
+                        'id' => self::TAGS[1]['id'],
+                        'name' => self::TAGS[1]['name'],
+                    ],
                 ],
             ],
         ];
