aa6f0e4dbeda5556fd53e88f0ff4aca02b6e7ef7
YuriNakatsugawa <nakatsugawa.yr@gmail.com>
2023-01-05 01:48:14 +0900

    style: 各ページのCSS微調整/変数設定/mixinファイルの追加

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700;900&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
